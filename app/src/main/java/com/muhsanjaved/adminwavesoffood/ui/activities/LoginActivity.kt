a29182069110e225405c7ea9e00f5fd5cc0a2b72
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-08-02 11:16:11 +0500

    login Activity is completed

--- app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/activities/LoginActivity.kt
@@ -1,12 +1,28 @@
 package com.muhsanjaved.adminwavesoffood.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.muhsanjaved.adminwavesoffood.R
+import com.muhsanjaved.adminwavesoffood.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private val binding: ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.textViewCreateNewAccount.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/activities/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.muhsanjaved.adminwavesoffood.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.muhsanjaved.adminwavesoffood.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
