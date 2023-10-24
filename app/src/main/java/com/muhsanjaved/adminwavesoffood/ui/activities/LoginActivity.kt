cf6a7c7e1631ea90d3e06bdfcf975a615f369970
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-24 12:58:36 +0500

    Login in FirebaseAuth Email password And Save User Data in Firebase RealTime Database

--- app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/activities/LoginActivity.kt
@@ -3,10 +3,25 @@ package com.muhsanjaved.adminwavesoffood.ui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.database
 import com.muhsanjaved.adminwavesoffood.R
 import com.muhsanjaved.adminwavesoffood.databinding.ActivityLoginBinding
+import com.muhsanjaved.adminwavesoffood.models.UserModel
+import com.muhsanjaved.adminwavesoffood.ui.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var email:String
+    private lateinit var password :String
+    private lateinit var auth:FirebaseAuth
+    private lateinit var database:DatabaseReference
 
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
@@ -14,15 +29,73 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(binding.root)
 
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        // Initialize Firebase Database
+        database = Firebase.database.reference
+
+        // Login Button
         binding.loginButton.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            email = binding.editTextLoginTextEmailAddress.text.toString().trim()
+            password=binding.editTextLoginTextPassword.text.toString().trim()
+
+            if (email.isBlank() || password.isBlank()){
+                Toast.makeText(this,"Please Fill All Details",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                createUserAccount(email,password)
+            }
+
+//            val intent = Intent(this, SignUpActivity::class.java)
+//            startActivity(intent)
         }
         binding.textViewCreateNewAccount.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun createUserAccount(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task ->
+            if (task.isSuccessful){
+                val user : FirebaseUser? = auth.currentUser
+                updateUI(user)
+            }
+            else{
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener { task->
+                    if (task.isSuccessful){
+                        val user : FirebaseUser? = auth.currentUser
+                        saveUserData()
+                        updateUI(user)
+                    }
+                    else{
+                        Toast.makeText(this,"Authentication failed",Toast.LENGTH_SHORT).show()
+                        Log.d("Account","CreateUserAccount: Authentication Failed",task.exception)
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun saveUserData() {
+
+        // get text form edittext
+        email = binding.editTextLoginTextEmailAddress.text.toString().trim()
+        password=binding.editTextLoginTextPassword.text.toString().trim()
+
+        val user  = UserModel(email,password)
+        val userId:String? = FirebaseAuth.getInstance().currentUser?.uid
+        userId?.let {
+            database.child("user").child(it).setValue(user)
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 }
