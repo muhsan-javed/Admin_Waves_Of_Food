04a5343d14d64fa8539fbe52464e359af06d3f5d
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-25 13:30:51 +0500

    code clear

--- app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/activities/SignUpActivity.kt
+++ app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/activities/SignUpActivity.kt
@@ -18,12 +18,12 @@ import com.muhsanjaved.adminwavesoffood.models.UserModel
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var auth : FirebaseAuth
-    private lateinit var email : String
-    private lateinit var password : String
-    private lateinit var userName :String
-    private lateinit var nameOfRestaurant :String
-    private lateinit var database : DatabaseReference
+    private lateinit var auth: FirebaseAuth
+    private lateinit var email: String
+    private lateinit var password: String
+    private lateinit var userName: String
+    private lateinit var nameOfRestaurant: String
+    private lateinit var database: DatabaseReference
 
     private val binding: ActivitySignUpBinding by lazy {
         ActivitySignUpBinding.inflate(layoutInflater)
@@ -57,10 +57,10 @@ class SignUpActivity : AppCompatActivity() {
             email = binding.editTextSignUpTextEmailAddress.text.toString().trim()
             password = binding.editTextSignUpTextPassword.text.toString().trim()
 
-            if (userName.isBlank() || nameOfRestaurant.isBlank() || email.isBlank() || password.isBlank()){
-                Toast.makeText(this,"Please fill All Details",Toast.LENGTH_LONG).show()
-            }else {
-                createAccount(email,password)
+            if (userName.isBlank() || nameOfRestaurant.isBlank() || email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Please fill All Details", Toast.LENGTH_LONG).show()
+            } else {
+                createAccount(email, password)
             }
 
 //            val intent = Intent(this, MainActivity::class.java)
@@ -70,17 +70,15 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun createAccount(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
-            task->
-            if (task.isSuccessful){
-                Toast.makeText(this,"Account Created Successfully",Toast.LENGTH_SHORT).show()
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Toast.makeText(this, "Account Created Successfully", Toast.LENGTH_SHORT).show()
                 saveUserData()
                 val intent = Intent(this, LoginActivity::class.java)
                 startActivity(intent)
                 finish()
-            }
-            else {
-                Toast.makeText(this,"Account Creation Failed",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Account Creation Failed", Toast.LENGTH_SHORT).show()
                 Log.d("Account", "createAccount: Failed", task.exception)
             }
 
@@ -95,8 +93,8 @@ class SignUpActivity : AppCompatActivity() {
         email = binding.editTextSignUpTextEmailAddress.text.toString().trim()
         password = binding.editTextSignUpTextPassword.text.toString().trim()
 
-        val user = UserModel(userName,nameOfRestaurant,email,password)
-        val userId : String = FirebaseAuth.getInstance().currentUser!!.uid
+        val user = UserModel(userName, nameOfRestaurant, email, password)
+        val userId: String = FirebaseAuth.getInstance().currentUser!!.uid
         // Save user data Firebase Database
         database.child("user").child(userId).setValue(user)
     }
