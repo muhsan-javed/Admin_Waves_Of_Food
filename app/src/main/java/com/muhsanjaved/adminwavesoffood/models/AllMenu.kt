fb8e25e3db082728196384946443790b5db24128
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-31 12:13:23 +0500

    new AllMenu Model

---
+++ app/src/main/java/com/muhsanjaved/adminwavesoffood/models/AllMenu.kt
@@ -0,0 +1,9 @@
+package com.muhsanjaved.adminwavesoffood.models
+
+data class AllMenu (
+    val foodName:String? = null,
+    val foodPrice:String? = null,
+    val foodDescription:String? = null,
+    val foodIngredient:String? = null,
+    val foodImage:String? = null,
+)
