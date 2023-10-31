2ede6034b28a9ecf52e1c6be6924ef87c9dfb482
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-10-31 11:21:57 +0500

    firebase signOut code

--- app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/MainActivity.kt
+++ app/src/main/java/com/muhsanjaved/adminwavesoffood/ui/MainActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.muhsanjaved.adminwavesoffood.databinding.ActivityMainBinding
 import com.muhsanjaved.adminwavesoffood.ui.activities.AddItemActivity
 import com.muhsanjaved.adminwavesoffood.ui.activities.AdminProfileActivity
@@ -15,12 +18,16 @@ import com.muhsanjaved.adminwavesoffood.ui.activities.PendingOrdersActivity
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         binding.addMenuButton.setOnClickListener {
             val intent = Intent(this, AddItemActivity::class.java)
@@ -51,6 +58,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.logOutButton.setOnClickListener {
             Toast.makeText(this, "Your Are LoyOut", Toast.LENGTH_SHORT).show()
+            Firebase.auth.signOut()
         }
 
     }
