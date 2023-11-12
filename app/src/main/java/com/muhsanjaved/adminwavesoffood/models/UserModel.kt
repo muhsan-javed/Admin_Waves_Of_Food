76fd51c4ac4491279e7421ee57f0caa874b7fb8c
Muhsan Javed <67718185+muhsan-javed@users.noreply.github.com>
2023-11-12 16:29:52 +0500

    add address or phone

--- app/src/main/java/com/muhsanjaved/adminwavesoffood/models/UserModel.kt
+++ app/src/main/java/com/muhsanjaved/adminwavesoffood/models/UserModel.kt
@@ -4,5 +4,7 @@ data class UserModel(
     val name:String? = null,
     val nameOfRestaurant:String? = null,
     val email:String? = null,
-    val password:String? = null
+    val password:String? = null,
+    val address:String? = null,
+    val phone:String? = null,
 )
